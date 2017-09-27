31e44bef202bc537f9540989a5efeaccece7b3c3
suy0ng <s.goto823@gmail.com>
2017-09-27 20:31:45 +0900

    Add 4.2

---
+++ python/AutomateBoringStuff/ch4/allMyCats2.py
@@ -0,0 +1,10 @@
+cat_names = []
+while True:
+    print('猫' + str(len(cat_names) + 1) + 'の名前を入力してください' + '(終了するにはEnterキーだけ押して下さい)')
+    name = input()
+    if name == '':
+        break
+    cat_names = cat_names + [name]
+print('猫の名前は次の通り:')
+for name in cat_names:
+    print(' ' + name)
